5fa7b762699d57f1f967f326e5456695117c3c62
Kevin Digan <kd073@hotmail.com>
2021-09-16 21:41:26 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function introduction(name = "Josh") {
+    return (`Hi, my name is ${name}.`);
+}
+
+function introductionWithLanguage(name = "Josh", language = "ember.js") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+
+function introductionWithLanguageOptional(name = "Josh", language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+
+const name = "Gracie"
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+
+}
